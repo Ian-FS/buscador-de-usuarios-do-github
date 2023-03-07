f413eab7e7bf2bea20d39c24d3e6c5dd751990f8
Ian_França <iiann_2009@hotmail.com>
2023-03-07 17:33:39 -0300

    atualizado a lógica no tratamento dos dados recebidos

--- app-code/main.js
+++ app-code/main.js
@@ -1,5 +1,38 @@
 let imgUser = document.querySelector("#img-user");
+let infoUser = document.querySelector(".info");
+let bibliography = document.querySelector(".bibliography");
 
 fetch("https://api.github.com/users/octocat")
   .then((res) => res.json())
-  .then((data) => (imgUser.src = data.avatar_url));
+  .then((data) => {
+    //adiciona imagem do usuario
+    imgUser.src = data.avatar_url;
+
+    //adciona nome do usario
+    infoUser.children[0].innerHTML = data.name;
+
+    //adiciona link, caso tenha, ou nome com @
+    infoUser.children[1].innerHTML = `@${data.login}`;
+
+    //adiciona data de criação de conta
+    const meses = [
+      "Jan",
+      "Fev",
+      "Mar",
+      "Abr",
+      "Mai",
+      "Jun",
+      "Jul",
+      "Ago",
+      "Set",
+      "Out",
+      "Nov",
+      "Dez",
+    ];
+    let date = new Date(data.created_at);
+    let dataFormatada =
+      date.getDate() + " " + meses[date.getMonth()] + " " + date.getFullYear();
+    infoUser.children[2].innerHTML = `Joined ${dataFormatada}`;
+
+    //adiciona bibliografia
+  });
