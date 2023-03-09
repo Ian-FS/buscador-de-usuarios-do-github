9113b707edc384865fb9ecbf8ce865cc3be5d205
Ian_França <iiann_2009@hotmail.com>
2023-03-09 18:55:57 -0300

    adicionado logica de pesquisa para encontrar usuarios

--- app-code/main.js
+++ app-code/main.js
@@ -1,9 +1,8 @@
 let imgUser = document.querySelector("#img-user");
 let infoUser = document.querySelector(".info");
 let bibliography = document.querySelector(".bibliography");
-console.log(bibliography);
 
-fetch("https://api.github.com/users/ian-fs")
+fetch(`https://api.github.com/users/octocat`)
   .then((res) => res.json())
   .then((data) => {
     //adiciona imagem do usuario
@@ -42,3 +41,51 @@ fetch("https://api.github.com/users/ian-fs")
       bibliography.children[0].innerHTML = data.bio;
     }
   });
+
+function pesquisar() {
+  nomeUsuario = document.querySelector("#campo-pesquisa").value;
+  console.log(nomeUsuario);
+  fetch(`https://api.github.com/users/${nomeUsuario}`)
+    .then((res) => res.json())
+    .then((data) => {
+      //adiciona imagem do usuario
+      imgUser.src = data.avatar_url;
+
+      //adciona nome do usario
+      infoUser.children[0].innerHTML = data.name;
+
+      //adiciona link, caso tenha, ou nome com @
+      infoUser.children[1].innerHTML = `@${data.login}`;
+
+      //adiciona data de criação de conta
+      const meses = [
+        "Jan",
+        "Fev",
+        "Mar",
+        "Abr",
+        "Mai",
+        "Jun",
+        "Jul",
+        "Ago",
+        "Set",
+        "Out",
+        "Nov",
+        "Dez",
+      ];
+      let date = new Date(data.created_at);
+      let dataFormatada =
+        date.getDate() +
+        " " +
+        meses[date.getMonth()] +
+        " " +
+        date.getFullYear();
+      infoUser.children[2].innerHTML = `Joined ${dataFormatada}`;
+
+      //adiciona bibliografia
+      if (data.bio === null) {
+        bibliography.children[0].innerHTML = "Este perfil não tem biografia.";
+      } else {
+        bibliography.children[0].innerHTML = data.bio;
+      }
+    });
+}
