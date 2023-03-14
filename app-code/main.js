a579e0953faff69c558cec72ade1ef5a26375aa6
Ian_França <iiann_2009@hotmail.com>
2023-03-14 12:16:47 -0300

    atualizado a logica de pesquisa e adicionado a funcionalidade de fazer a pesquisa apertando o botao enter

--- app-code/main.js
+++ app-code/main.js
@@ -1,69 +1,27 @@
-let imgUser = document.querySelector("#img-user");
-let nameUser = document.querySelector("#name-user");
-let linkUser = document.querySelector("#link-user");
-let dateJoined = document.querySelector("#date-joined");
-let infoUser = document.querySelector(".info");
-let bibliography = document.querySelector(".bibliography");
-let repositorios = document.querySelector(".repos");
-let followers = document.querySelector(".followers");
-let following = document.querySelector(".following");
-let locationUser = document.querySelector(".location-user");
-let blog = document.querySelector(".blog");
-let twitter = document.querySelector(".twitter");
-let company = document.querySelector(".company");
+const imgUser = document.querySelector("#img-user");
+const nameUser = document.querySelector("#name-user");
+const linkUser = document.querySelector("#link-user");
+const dateJoined = document.querySelector("#date-joined");
+const infoUser = document.querySelector(".info");
+const bibliography = document.querySelector(".bibliography");
+const repositories = document.querySelector(".repos");
+const followers = document.querySelector(".followers");
+const following = document.querySelector(".following");
+const locationUser = document.querySelector(".location-user");
+const blog = document.querySelector(".blog");
+const twitter = document.querySelector(".twitter");
+const company = document.querySelector(".company");
+const buttonSearch = document.querySelector("#button-search");
+var researchField = document.querySelector("#research-field");
 
-fetch(`https://api.github.com/users/octocat`)
-  .then((res) => res.json())
-  .then((data) => {
-    //adiciona imagem do usuario
-    imgUser.src = data.avatar_url;
+researchField.value = "octocat";
+onload = search();
+researchField.addEventListener("keypress", (ev) => {
+  if (ev.key === "Enter") search();
+});
 
-    //adciona nome do usario
-    nameUser.innerHTML = data.name;
-
-    //adiciona link, caso tenha, ou nome com @
-    linkUser.innerHTML = `@${data.login}`;
-
-    //adiciona data de criação de conta
-    const meses = [
-      "Jan",
-      "Fev",
-      "Mar",
-      "Abr",
-      "Mai",
-      "Jun",
-      "Jul",
-      "Ago",
-      "Set",
-      "Out",
-      "Nov",
-      "Dez",
-    ];
-    let date = new Date(data.created_at);
-    let dataFormatada =
-      date.getDate() + " " + meses[date.getMonth()] + " " + date.getFullYear();
-    dateJoined.innerHTML = `Joined ${dataFormatada}`;
-
-    //adiciona bibliografia
-    if (data.bio === null) {
-      bibliography.children[0].innerHTML = "Este perfil não tem biografia.";
-    } else {
-      bibliography.children[0].innerHTML = data.bio;
-    }
-    //adiciona outras informações
-    repositorios.children[1].innerHTML = data.public_repos;
-    followers.children[1].innerHTML = data.followers;
-    following.children[1].innerHTML = data.following;
-    locationUser.children[1].innerHTML = data.location;
-    blog.children[1].innerHTML = data.blog;
-    twitter.children[1].innerHTML = data.twitter;
-    company.children[1].innerHTML = data.company;
-  });
-
-function pesquisar() {
-  nomeUsuario = document.querySelector("#campo-pesquisa").value;
-  console.log(nomeUsuario);
-  fetch(`https://api.github.com/users/${nomeUsuario}`)
+function search() {
+  fetch(`https://api.github.com/users/${researchField.value}`)
     .then((res) => res.json())
     .then((data) => {
       //adiciona imagem do usuario
@@ -107,7 +65,7 @@ function pesquisar() {
       }
 
       //adiciona informações do box
-      repositorios.children[1].innerHTML = data.public_repos;
+      repositories.children[1].innerHTML = data.public_repos;
       followers.children[1].innerHTML = data.followers;
       following.children[1].innerHTML = data.following;
 
