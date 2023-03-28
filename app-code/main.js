c32032006469d24528bc99294f95a04c272bf44a
Ian_França <iiann_2009@hotmail.com>
2023-03-28 13:16:44 -0300

    adicionado funcionalidade que identifica a preferencia de tema do device do usuario e aplica o tema a pagina

--- app-code/main.js
+++ app-code/main.js
@@ -13,7 +13,9 @@ const twitter = document.querySelector(".twitter");
 const company = document.querySelector(".company");
 const buttonSearch = document.querySelector("#button-search");
 var researchField = document.querySelector("#research-field");
-const tema = document.querySelector("#tema");
+const preferDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const preferLight = window.matchMedia("(prefers-color-scheme: light)").matches;
+console.log(preferLight);
 const sun =
   "M13.545 6.455c-.9-.9-2.17-1.481-3.545-1.481a4.934 4.934 0 00-3.545 1.481c-.9.9-1.481 2.17-1.481 3.545 0 1.376.582 2.646 1.481 3.545.9.9 2.17 1.481 3.545 1.481a4.934 4.934 0 003.545-1.481c.9-.9 1.481-2.17 1.481-3.545a4.934 4.934 0 00-1.481-3.545zM10 3.413a.7.7 0 00.688-.688V.688A.7.7 0 0010 0a.7.7 0 00-.688.688v2.037a.7.7 0 00.688.688zM15.635 5.344l1.455-1.455a.67.67 0 000-.952.67.67 0 00-.952 0l-1.455 1.455a.67.67 0 000 .952c.238.264.66.264.952 0zM19.312 9.312h-2.037a.7.7 0 00-.688.688.7.7 0 00.688.688h2.037A.7.7 0 0020 10a.7.7 0 00-.688-.688zM15.608 14.656a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455a.67.67 0 00.952 0 .67.67 0 000-.952l-1.455-1.455zM10 16.587a.7.7 0 00-.688.688v2.037A.7.7 0 0010 20a.7.7 0 00.688-.688v-2.037a.7.7 0 00-.688-.688zM4.365 14.656L2.91 16.111a.67.67 0 000 .952.67.67 0 00.952 0l1.455-1.455a.67.67 0 000-.952c-.238-.264-.66-.264-.952 0zM3.413 10a.7.7 0 00-.688-.688H.688A.7.7 0 000 10a.7.7 0 00.688.688h2.037A.7.7 0 003.413 10zM4.365 5.344a.67.67 0 00.952 0 .67.67 0 000-.952L3.862 2.937a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455z";
 const moon =
@@ -35,7 +37,7 @@ function load() {
 }
 
 function alteraTema() {
-  if (tema.children[0].innerHTML === "DARK") {
+  if (preferDark) {
     document.getElementById("tema").children[0].classList.remove("dark-h2");
     document.getElementById("icone").classList.remove("dark-icone");
     temaDark();
