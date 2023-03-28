008c1ca7dced3a26369e44ea2dcad688c4cd1e1a
Ian_França <iiann_2009@hotmail.com>
2023-03-28 13:01:41 -0300

    atualizado logica e visual da funcionalidade de alterar tema entre dark e light

--- app-code/main.js
+++ app-code/main.js
@@ -14,12 +14,13 @@ const company = document.querySelector(".company");
 const buttonSearch = document.querySelector("#button-search");
 var researchField = document.querySelector("#research-field");
 const tema = document.querySelector("#tema");
-console.log(
-  document.querySelector("#icone").children[0].children[0].attributes[1].value
-);
+const sun =
+  "M13.545 6.455c-.9-.9-2.17-1.481-3.545-1.481a4.934 4.934 0 00-3.545 1.481c-.9.9-1.481 2.17-1.481 3.545 0 1.376.582 2.646 1.481 3.545.9.9 2.17 1.481 3.545 1.481a4.934 4.934 0 003.545-1.481c.9-.9 1.481-2.17 1.481-3.545a4.934 4.934 0 00-1.481-3.545zM10 3.413a.7.7 0 00.688-.688V.688A.7.7 0 0010 0a.7.7 0 00-.688.688v2.037a.7.7 0 00.688.688zM15.635 5.344l1.455-1.455a.67.67 0 000-.952.67.67 0 00-.952 0l-1.455 1.455a.67.67 0 000 .952c.238.264.66.264.952 0zM19.312 9.312h-2.037a.7.7 0 00-.688.688.7.7 0 00.688.688h2.037A.7.7 0 0020 10a.7.7 0 00-.688-.688zM15.608 14.656a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455a.67.67 0 00.952 0 .67.67 0 000-.952l-1.455-1.455zM10 16.587a.7.7 0 00-.688.688v2.037A.7.7 0 0010 20a.7.7 0 00.688-.688v-2.037a.7.7 0 00-.688-.688zM4.365 14.656L2.91 16.111a.67.67 0 000 .952.67.67 0 00.952 0l1.455-1.455a.67.67 0 000-.952c-.238-.264-.66-.264-.952 0zM3.413 10a.7.7 0 00-.688-.688H.688A.7.7 0 000 10a.7.7 0 00.688.688h2.037A.7.7 0 003.413 10zM4.365 5.344a.67.67 0 00.952 0 .67.67 0 000-.952L3.862 2.937a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455z";
+const moon =
+  "M19.513 11.397a.701.701 0 00-.588.128 7.496 7.496 0 01-2.276 1.336 7.101 7.101 0 01-2.583.462 7.505 7.505 0 01-5.32-2.209 7.568 7.568 0 01-2.199-5.342c0-.873.154-1.72.41-2.49a6.904 6.904 0 011.227-2.21.657.657 0 00-.102-.924.701.701 0 00-.589-.128C5.32.61 3.427 1.92 2.072 3.666A10.158 10.158 0 000 9.83c0 2.8 1.125 5.342 2.967 7.19a10.025 10.025 0 007.16 2.98c2.353 0 4.527-.822 6.266-2.183a10.13 10.13 0 003.58-5.624.623.623 0 00-.46-.796z";
 
 researchField.value = "octocat";
-onload = search();
+onload = load();
 researchField.addEventListener("keypress", (ev) => {
   if (ev.key === "Enter") {
     search();
@@ -28,18 +29,27 @@ researchField.addEventListener("keypress", (ev) => {
 
 tema.addEventListener("click", alteraTema);
 
+function load() {
+  search();
+  alteraTema();
+}
+
 function alteraTema() {
   if (tema.children[0].innerHTML === "DARK") {
-    tema.classList.add("dark");
+    document.getElementById("tema").children[0].classList.remove("dark-h2");
+    document.getElementById("icone").classList.remove("dark-icone");
+    temaDark();
+  } else {
+    document.getElementById("tema").children[0].classList.remove("light-h2");
+    document.getElementById("icone").classList.remove("light-icone");
+    temaLight();
+  }
+
+  function temaDark() {
+    document.getElementById("tema").children[0].classList.add("light-h2");
+    document.getElementById("icone").classList.add("light-icone");
     tema.children[0].innerHTML = "LIGTH";
-    // tema.children[0].style.color = "#FFFFFF";
-    document.querySelector(
-      "#icone"
-    ).children[0].children[0].attributes[1].value = "#FFFFFF";
-    document.querySelector(
-      "#icone"
-    ).children[0].children[0].attributes[0].value =
-      "M13.545 6.455c-.9-.9-2.17-1.481-3.545-1.481a4.934 4.934 0 00-3.545 1.481c-.9.9-1.481 2.17-1.481 3.545 0 1.376.582 2.646 1.481 3.545.9.9 2.17 1.481 3.545 1.481a4.934 4.934 0 003.545-1.481c.9-.9 1.481-2.17 1.481-3.545a4.934 4.934 0 00-1.481-3.545zM10 3.413a.7.7 0 00.688-.688V.688A.7.7 0 0010 0a.7.7 0 00-.688.688v2.037a.7.7 0 00.688.688zM15.635 5.344l1.455-1.455a.67.67 0 000-.952.67.67 0 00-.952 0l-1.455 1.455a.67.67 0 000 .952c.238.264.66.264.952 0zM19.312 9.312h-2.037a.7.7 0 00-.688.688.7.7 0 00.688.688h2.037A.7.7 0 0020 10a.7.7 0 00-.688-.688zM15.608 14.656a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455a.67.67 0 00.952 0 .67.67 0 000-.952l-1.455-1.455zM10 16.587a.7.7 0 00-.688.688v2.037A.7.7 0 0010 20a.7.7 0 00.688-.688v-2.037a.7.7 0 00-.688-.688zM4.365 14.656L2.91 16.111a.67.67 0 000 .952.67.67 0 00.952 0l1.455-1.455a.67.67 0 000-.952c-.238-.264-.66-.264-.952 0zM3.413 10a.7.7 0 00-.688-.688H.688A.7.7 0 000 10a.7.7 0 00.688.688h2.037A.7.7 0 003.413 10zM4.365 5.344a.67.67 0 00.952 0 .67.67 0 000-.952L3.862 2.937a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455z";
+    document.querySelector("#icone").children[0].attributes[0].value = sun;
     document.querySelector(".logo").style.color = "#FFFFFF";
     document.querySelector("body").style.backgroundColor = "#141D2F";
     document.querySelector("#research-field").style.backgroundColor = "#1E2A47";
@@ -82,15 +92,13 @@ function alteraTema() {
     document.querySelector(
       ".box-other-info"
     ).children[3].children[0].style.filter = "brightness(4.4)";
-  } else {
-    // tema.dataset.filter = "brightness(100%)";
-    tema.children[0].innerHTML = "DARK";
-    // tema.children[0].style.color = "#4B6A9B";
+  }
 
-    document.querySelector(
-      "#icone"
-    ).children[0].children[0].attributes[0].value =
-      "M19.513 11.397a.701.701 0 00-.588.128 7.496 7.496 0 01-2.276 1.336 7.101 7.101 0 01-2.583.462 7.505 7.505 0 01-5.32-2.209 7.568 7.568 0 01-2.199-5.342c0-.873.154-1.72.41-2.49a6.904 6.904 0 011.227-2.21.657.657 0 00-.102-.924.701.701 0 00-.589-.128C5.32.61 3.427 1.92 2.072 3.666A10.158 10.158 0 000 9.83c0 2.8 1.125 5.342 2.967 7.19a10.025 10.025 0 007.16 2.98c2.353 0 4.527-.822 6.266-2.183a10.13 10.13 0 003.58-5.624.623.623 0 00-.46-.796z";
+  function temaLight() {
+    document.getElementById("tema").children[0].classList.add("dark-h2");
+    document.getElementById("icone").classList.add("dark-icone");
+    tema.children[0].innerHTML = "DARK";
+    document.querySelector("#icone").children[0].attributes[0].value = moon;
     document.querySelector(".logo").style.color = "#222731";
     document.querySelector("body").style.backgroundColor = "#F2F2F2";
     document.querySelector("#research-field").style.backgroundColor = "#FFFFFF";
